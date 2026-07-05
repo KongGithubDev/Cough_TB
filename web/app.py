5b6d49bb5781aee289fa58efc13129f5e79d5d1f
Watcharapong Namsaeng <kongwatcharapongemail@gmail.com>
2026-07-06 00:31:07 +0700

    Fix TemplateResponse signature for Starlette 1.x

--- web/app.py
+++ web/app.py
@@ -172,7 +172,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request, "index.html")
 
 @app.post("/predict")
 async def predict_endpoint(file: UploadFile = File(...)):
